b54fa8757de20ab43e0081b870025dff7ab4f28f
Badtz Maru <badtzmaru139@gmail.com>
2016-09-07 18:40:40 -0700

    i think i added adblock

--- main.js
+++ main.js
@@ -57,7 +57,4 @@ app.on('activate', function () {
 })
 
 // In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
-
-// install adblock
-BrowserWindow.addDevToolsExtension("/ext/uBlock0.chromium")
+// code. You can also put them in separate files and require them here.=
--- renderer.js
+++ renderer.js
@@ -1,3 +1,12 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
+
+import installExtension, {
+  EMBER_INSPECTOR, REACT_DEVELOPER_TOOLS,
+  BACKBONE_DEBUGGER, JQUERY_DEBUGGER,
+  ANGULARJS_BATARANG, VUEJS_DEVTOOLS,
+  REDUX_DEVTOOLS, REACT_PERF,
+} from 'electron-devtools-installer';
+
+installExtension("cjpalhdlnbpafiamejdnhcphjbkeiagm")

8c9c805e3611cc29d8e1d9aa2f8e18dff0f87361
PanDoes Dalton <daltonl@frontier.com>
2016-09-19 15:20:32 -0700

    buttons, working.

--- default.js
+++ default.js
@@ -21,18 +21,29 @@
         const window = remote.getCurrentWindow();
         window.close();
       });
+      document.getElementById("stream-btn").addEventListener("click", function (e) {
+        document.getElementById("main-view").executeJavaScript("document.getElementsByClassName('header__mainMenu-loggedInHome header__mainMenu-stream')[0].click()")
+      });
+
+      document.getElementById("profile-btn").addEventListener("click", function (e) {
+        document.getElementById("main-view").executeJavaScript("document.getElementsByClassName('userNav__button userNav__usernameButton')[0].click(); document.getElementsByClassName('profileMenu__link profileMenu__profile')[0].click()")
+      });
 
       document.getElementById("like-btn").addEventListener("click", function (e) {
         document.getElementById("main-view").executeJavaScript("document.getElementsByClassName('userNav__button userNav__usernameButton')[0].click(); document.getElementsByClassName('profileMenu__link profileMenu__likes')[0].click()")
       });
 
+      document.getElementById("follow-btn").addEventListener("click", function (e) {
+        document.getElementById("main-view").executeJavaScript("document.getElementsByClassName('userNav__button userNav__usernameButton')[0].click(); document.getElementsByClassName('profileMenu__link profileMenu__following')[0].click()")
+      });
+
       document.getElementById("playlist-btn").addEventListener("click", function (e) {
         document.getElementById("main-view").executeJavaScript("document.getElementsByClassName('userNav__button userNav__usernameButton')[0].click(); document.getElementsByClassName('profileMenu__link profileMenu__sets')[0].click()")
       });
-
-      document.getElementById("friend-btn").addEventListener("click", function (e) {
-        document.getElementById("main-view").executeJavaScript("document.getElementsByClassName('userNav__button userNav__usernameButton')[0].click(); document.getElementsByClassName('profileMenu__link profileMenu__following')[0].click()")
-      });
+      /*
+      document.getElementById("stats-btn").addEventListener("click", function (e) {
+        document.getElementById("main-view").executeJavaScript("document.getElementsByClassName('userNav__button userNav__usernameButton')[0].click(); document.getElementsByClassName('profileMenu__link profileMenu__')[0].click()")
+      });*/
     };
 
     document.onreadystatechange = function () {

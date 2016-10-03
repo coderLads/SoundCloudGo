bb67b4291356357b106433efbdd1adaffb11e1b7
Badtz Maru <badtzmaru139@gmail.com>
2016-10-03 11:06:01 -0700

    added a back button

--- default.js
+++ default.js
@@ -3,6 +3,9 @@
   const remote = require('electron').remote;
 
   function init() {
+    document.getElementById("back-btn").addEventListener("click", function (e) {
+        document.getElementById("main-view").goBack();
+    });
     document.getElementById("min-btn").addEventListener("click", function (e) {
       const window = remote.getCurrentWindow();
       window.minimize();
